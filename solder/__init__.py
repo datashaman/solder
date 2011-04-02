190bfcc57113baac00091b1c0ab2f026521b6330
Marlin Forbes <marlinf@datashaman.com>
2011-04-02 18:19:21 +0200

    Whee. Got the table page working at /users. The rest should follow suit... hopefully...

--- solder/__init__.py
+++ solder/__init__.py
@@ -1,20 +1,16 @@
-import collections, os, textwrap, re, sys
-from pyquery import PyQuery
-from lxml.etree import fromstring
+import collections, os, textwrap, sys, logging
+from pprint import pprint
 from routes import Mapper
 from routes.middleware import RoutesMiddleware
 from solder.models.auth import make_users, User
 from paste.util.import_string import simple_import
 from paste.httpexceptions import *
 
-from welder import weld as w
-def pyquery_weld(data, config={}):
-    w(this[0], data, config)
-    return this
+from solder.template import Template
 
-PyQuery.fn.weld = pyquery_weld
+log = logging.getLogger(__name__)
 
-# make_users(10)
+make_users(10)
 
 def make_auth_middleware(wrap, global_conf, **app_conf):
     from repoze.who.config import make_middleware_with_config
@@ -26,7 +22,7 @@ def make_auth_middleware(wrap, global_conf, **app_conf):
 
 def route_map():
     m = Mapper()
-    m.collection('users', 'user', 'user')
+    m.connect('/favicon.ico', controller='null')
     m.connect('/', controller='user')
     return m
 
@@ -42,74 +38,22 @@ class App(object):
 
     def __call__(self, environ, start_response):
         url, routing_dict = environ['wsgiorg.routing_args']
-        if 'controller' in routing_dict:
-            source = 'solder/controllers/%s.py' % routing_dict['controller']
-            template = 'solder/views/table.html'
 
-            result = weld(template, source)
+        status = '200 OK'
+        headers = [('Content-Type', 'text/html')]
+        start_response(status, headers)
 
-            status = '200 OK'
-            headers = [('Content-Type', 'text/html')]
-            start_response(status, headers)
+        if environ['PATH_INFO'] == '/favicon.ico':
+            return ''
 
-            return result
-        else:
-            response = HTTPNotFound()
+        template = Template.parse('solder/controllers/user.py')
 
-        return response(environ, start_response)
+        html = 'solder/views/table.html'
+        return template.render(html, environ)
 
-def weld(template_file, source_file):
-    with open(template_file) as f:
-        pq = PyQuery(f.read())
-
-    template_basename = os.path.basename(template_file).split('.')[-2]
-
-    glob = globals()
-    local = locals()
-
-    with open(source_file) as f:
-        routes = {}
-        preamble = ''
-        pattern = None
-        route = None
-        selector = None
-        match = False
-        first = True
-
-        for l in f.readlines():
-            re_match = re.search(r'(.+):(.+)', l.strip())
-
-            if re_match:
-                template = re_match.group(1)
-                pattern = re_match.group(2)
-
-                match = template_basename == template
-
-                route = routes[pattern] = {}
-
-                if first:
-                    script = compile(preamble, '<preamble:%s>' % source_file, 'exec')
-                    exec(preamble, glob, local)
-                    first = False
-            else:
-                if route is None:
-                    preamble += l
-                elif l.startswith('  ') and match:
-                    m = re.search('(.*)\s+\<\s+(.*)', l.strip())
-                    if m:
-                        selector = m.group(1).strip()
-                        provider = m.group(2).strip()
-
-                        routes[pattern][selector] = provider
-
-                        script = compile(provider, '<selector:%s>' % selector, 'eval')
-                        data = eval(script, glob, local)
-
-                        pq(selector).weld(data)
-    return pq.__html__()
 
 def make_app(global_conf, **app_conf):
     app = wrap = App(global_conf, **app_conf)
     wrap = RoutesMiddleware(wrap, route_map())
-    wrap = make_auth_middleware(wrap, global_conf, **app_conf)
+    # wrap = make_auth_middleware(wrap, global_conf, **app_conf)
     return wrap
--- solder/controllers/user.py
+++ solder/controllers/user.py
@@ -1,13 +1,23 @@
 from solder.models.auth import User
-objects = User.objects.all()
+users = User.objects.all()
 
-columns = [
-    ('username', 'User Name'),
+columns = (
+    ('username', 'Username'),
     ('password', 'Password'),
     ('email', 'Email Address'),
-]
+)
 
-table:/users
-  #table thead th         < [label for _, label in columns]
-  #table tbody td         < [fromstring('<span class="%s"/>' % key) for key, _ in columns]
-  #table tbody tr         < [obj.attributes_dict for obj in objects]
+user = lambda username: User.objects.filter(username=username).first()
+
+*table(objects, columns, title):
+    title               is title
+    #table thead th     is [label for _, label in columns]
+    #table tbody td     is [fromstring('<span class="%s"/>' % key) for key, _ in columns]
+    #table tbody tr     is [obj.attributes_dict for obj in objects]
+
+*record(obj, title):
+    title               is title
+    #record tr          is obj.attributes_dict
+
+/users                       is *table(users, columns, 'Users')
+/user/(?P<username>.+)       is *record(user(username), 'User %s' % user(username).username)
